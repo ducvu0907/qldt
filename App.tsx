c59a5153b078905b1fc33e2699ca27e6d9fd1833
ducvu <ducvu973xd@gmail.com>
2024-11-03 23:56:27 +0700

    initial config setup

--- App.tsx
+++ App.tsx
@@ -1,20 +1,64 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, Text, View, FlatList } from 'react-native';
+import "./global.css";
+import { useState } from 'react';
+import axios from 'axios';
+import { QueryClient, QueryClientProvider, useQuery } from '@tanstack/react-query';
+
+export type Post = {
+  userId: number
+  id: number
+  title: string
+  body: string
+};
+
+const queryClient = new QueryClient();
+
+const fetchData = async (): Promise<Post[]> => {
+  const res = await axios.get("https://jsonplaceholder.typicode.com/posts");
+  return res.data;
+};
+
+const PostsList = () => {
+  const postsQuery = useQuery<Post[]>({
+    queryKey: ["posts"],
+    queryFn: fetchData
+  });
+
+  if (postsQuery.isLoading) {
+    return <Text>Loading...</Text>
+  }
+
+  if (postsQuery.isError) {
+    return <Text>Error...</Text>
+  }
+
+  return (
+    <FlatList
+      data={postsQuery.data}
+      keyExtractor={(item) => item.id.toString()}
+      renderItem={({ item }) => (
+        <View>
+          <Text className="font-bold">{item.title}</Text>
+          <Text>{item.body}</Text>
+        </View>
+      )}
+    />
+  );
+};
 
 export default function App() {
+  const [count, setCount] = useState<number>(0);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <QueryClientProvider client={queryClient}>
+      <View className="h-full flex justify-center">
+        <Text className="font-bold italic">Test Native Wind</Text>
+        <Button title="Increment" onPress={() => setCount(prev => prev + 1)} />
+        <Text>Count: {count}</Text>
+        <PostsList />
+        <StatusBar style="auto" />
+      </View>
+    </QueryClientProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
