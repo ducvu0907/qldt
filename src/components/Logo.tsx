9c76729b112ab30c45df8474c6a74ee85decfbc0
ducvu <ducvu973xd@gmail.com>
2024-11-12 23:19:52 +0700

    logo component

---
+++ src/components/Logo.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native";
+
+const Logo = () => {
+  return (
+    <View className="w-full bg-transparent flex items-center justify-center">
+      <Text className="font-bold text-white text-8xl shadow-md shadow-black">
+        HUST
+      </Text>
+    </View>
+  )
+};
+
+export default Logo;
