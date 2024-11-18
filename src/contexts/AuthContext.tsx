977ea807efcbfb0dd761e2f65090b9817a394189
ducvu <ducvu973xd@gmail.com>
2024-11-18 20:57:42 +0700

    add user role to auth context

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -5,11 +5,15 @@ import { AUTH_SERVER_URL } from "../types";
 interface AuthContextType {
   token: string | null,
   setToken: (token: string | null) => void;
+  role: string | null,
+  setRole: (role: string | null) => void;
 }
 
 const AuthContext = createContext<AuthContextType>({
   token: null,
-  setToken: () => {}
+  setToken: () => {},
+  role: null,
+  setRole: () => {},
 });
 
 interface AuthContextProviderProps {
@@ -18,11 +22,13 @@ interface AuthContextProviderProps {
 
 const AuthContextProvider: React.FC<AuthContextProviderProps> = ({ children }) => {
   const [token, setToken] = useState<string | null>(null);
+  const [role, setRole] = useState<string | null>(null);
 
   useEffect(() => {
     const fetchToken = async () => {
       const token = await SecureStorage.getItemAsync("access-token");
       if (token) {
+        console.log(token);
         setToken(token);
         // fetch to validate token or we logout to attain new token
         const res = await fetch(`${AUTH_SERVER_URL}/get_user_info`, {
@@ -30,12 +36,16 @@ const AuthContextProvider: React.FC<AuthContextProviderProps> = ({ children }) =
           headers: {
             'Content-Type': 'application/json',
           },
-          body: JSON.stringify({ "token": token }),
+          body: JSON.stringify({
+            token: token
+          }),
         });
         const data = await res.json();
         if (data.code !== 1000) {
           setToken(null);
         }
+        console.log("user role is ", data.data.role);
+        setRole(data.data.role);
       }
     }
 
@@ -43,7 +53,7 @@ const AuthContextProvider: React.FC<AuthContextProviderProps> = ({ children }) =
   }, []);
 
   return (
-    <AuthContext.Provider value={{ token, setToken }}>
+    <AuthContext.Provider value={{ token, setToken, role, setRole }}>
       {children}
     </AuthContext.Provider>
   );
