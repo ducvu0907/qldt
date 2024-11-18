479129d2fbf80658de1235fd9b796ed7bafee185
ducvu <ducvu973xd@gmail.com>
2024-11-18 15:52:45 +0700

    material stack

--- src/navigators/MaterialStack.tsx
+++ src/navigators/MaterialStack.tsx
@@ -1,5 +1,8 @@
 import { createStackNavigator } from "@react-navigation/stack";
 import MaterialList from "../screens/material/MaterialList";
+import UploadMaterial from "../screens/material/UploadMaterial";
+import MaterialInfo from "../screens/material/MaterialInfo";
+import EditMaterial from "../screens/material/EditMaterial";
 
 const Stack = createStackNavigator();
 
@@ -7,6 +10,9 @@ const MaterialStack = () => {
   return (
     <Stack.Navigator screenOptions={{headerShown: false}} initialRouteName="MaterialList">
       <Stack.Screen name="MaterialList" component={MaterialList} />
+      <Stack.Screen name="UploadMaterial" component={UploadMaterial} />
+      <Stack.Screen name="MaterialInfo" component={MaterialInfo} />
+      <Stack.Screen name="EditMaterial" component={EditMaterial} />
     </Stack.Navigator>
   );
 };
